9a2acc8fce05649a69d10f2a8b2d2b5e07175f72
Roy Jung <phi4grv@gmail.com>
2016-02-16 21:15:03 +0900

    Remove calling loadInfo, loadAuth to api server

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -6,27 +6,11 @@ import Navbar from 'react-bootstrap/lib/Navbar'
 import Nav from 'react-bootstrap/lib/Nav'
 import NavItem from 'react-bootstrap/lib/NavItem'
 import Helmet from 'react-helmet'
-import { isLoaded as isInfoLoaded, load as loadInfo } from 'redux/modules/info'
-import { isLoaded as isAuthLoaded, load as loadAuth, logout } from 'redux/modules/auth'
+import { logout } from 'redux/modules/auth'
 import { InfoBar } from 'components'
 import { routeActions } from 'react-router-redux'
 import config from '../../config'
-import { asyncConnect } from 'redux-async-connect'
 
-@asyncConnect([{
-  promise: ({store: {dispatch, getState}}) => {
-    const promises = []
-
-    if (!isInfoLoaded(getState())) {
-      promises.push(dispatch(loadInfo()))
-    }
-    if (!isAuthLoaded(getState())) {
-      promises.push(dispatch(loadAuth()))
-    }
-
-    return Promise.all(promises)
-  }
-}])
 @connect(
   state => ({user: state.auth.user}),
   {logout, pushState: routeActions.push})
