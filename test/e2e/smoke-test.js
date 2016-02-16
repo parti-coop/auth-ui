3e4be9a1efcd07043717fbdd349266d9375e8c5e
Roy Jung <phi4grv@gmail.com>
2016-02-16 21:23:30 +0900

    Add smoke_test 'API server is up' #7 [skip ci]

--- test/e2e/smoke-test.js
+++ test/e2e/smoke-test.js
@@ -1,12 +1,16 @@
-import { expect } from 'chai'
+import chai, { expect } from 'chai'
+import chaiAsPromised from 'chai-as-promised'
+chai.use(chaiAsPromised)
+
 import { equals as url_equals } from 'node-url-utils'
+import axios from 'axios'
 
 import config from '../../src/config';
 import { createBrowser } from './support/browser'
 import './support/setup-mocha'
 
-describe('Browser', () => {
-  it('connects to ui server', function *() {
+describe('UI server', () => {
+  it('is up', function *() {
     let port = process.env.AUTH_UI_PORT || config.port || 8080
     let server_url = `http://${config.host}:${port}`
 
@@ -15,5 +19,15 @@ describe('Browser', () => {
       .url()
 
     expect(url_equals(url, server_url)).to.be.true
+  })
+})
+
+describe('API server', () => {
+  it('is up', done => {
+    let port = process.env.AUTH_API_PORT || config.apiPort || 3030
+    let server_url = `http://${config.host}:${port}`
+    expect(
+      axios.get(server_url)
+    ).to.be.fulfilled.notify(done)
   })
 })
