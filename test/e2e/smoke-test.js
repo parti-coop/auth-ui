7bafbc011a57ece86a5120c34bc16695c7f38659
Roy Jung <phi4grv@gmail.com>
2016-02-16 16:24:53 +0900

    Clean up debug code

--- test/e2e/smoke-test.js
+++ test/e2e/smoke-test.js
@@ -6,16 +6,14 @@ import { createBrowser } from './support/browser'
 import './support/setup-mocha'
 
 describe('Browser', () => {
-  it('connects to test server', function *() {
+  it('connects to ui server', function *() {
     let port = process.env.AUTH_UI_PORT || config.port || 8080
     let server_url = `http://${config.host}:${port}`
-    console.log(server_url)
 
     let url = yield createBrowser()
       .goto(server_url)
       .url()
 
-    console.log(url)
     expect(url_equals(url, server_url)).to.be.true
   })
 })
