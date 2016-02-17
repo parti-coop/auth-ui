766180dc51297bf4506450a571899605b730cc25
Roy Jung <phi4grv@gmail.com>
2016-02-17 12:50:34 +0900

    Add helper script run-docker-compose.bash

--- deploy/docker/run-docker-compose.bash
+++ deploy/docker/run-docker-compose.bash
@@ -16,29 +16,11 @@ if [ -z "$AUTH_UI_VERSION" ]; then
     echo "AUTH_UI_VERSION does not exist."
     exit 1
 fi
-
-if [ -z "$AUTH_API_VERSION" ]; then
-    echo "AUTH_API_VERSION does not exist."
-    exit 1
-fi
+export AUTH_UI_VERSION=${AUTH_UI_VERSION:-latest}
 
 SCRIPT_DIR=$( script_dir )
 APP_HOME=$( dirname $( dirname $SCRIPT_DIR ) )
 
-DOCKER_COMPOSE_TEMPLTE_PATH=${SCRIPT_DIR}/docker-compose.yml.template
-DOCKER_COMPOSE_PATH=${SCRIPT_DIR}/docker-compose.yml
-export AUTH_UI_PORT=${AUTH_UI_PORT:-8080}
-export AUTH_API_PORT=${AUTH_API_PORT:-3030}
+export AUTH_API_VERSION=0.1.0-1-ge38f351
 
-TEMP_FILE=$( mktemp 2>/dev/null || mktemp -t 'auth-ui' )
-trap "rm -rf $TEMP_FILE $DOCKER_COMPOSE_PATH" EXIT
-trap "exit 1" 1 2 3 13 15
-
-(
-  echo 'cat <<EndOfDoc'
-  cat $DOCKER_COMPOSE_TEMPLTE_PATH
-  echo 'EndOfDoc'
-) > $TEMP_FILE
-. $TEMP_FILE > $DOCKER_COMPOSE_PATH
-
-docker-compose -f $DOCKER_COMPOSE_PATH "$@"
+${SCRIPT_DIR}/docker-compose.bash up
