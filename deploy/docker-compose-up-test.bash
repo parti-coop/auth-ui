d9ca67acc63999eb7a049c53022b0954a46777ec
Roy Jung <phi4grv@gmail.com>
2016-04-02 01:22:28 +1100

    Create docker network to allow bi-directional link

--- deploy/docker-compose-up-test.bash
+++ deploy/docker-compose-up-test.bash
@@ -15,9 +15,21 @@ script_dir() {
 SCRIPT_DIR=$( script_dir )
 ROOT_DIR=$( dirname $SCRIPT_DIR )
 
+AUTH_NETWORK=auth-net
+AUTH_NETWORK_ID=$( docker network ls -f name=${AUTH_NETWORK} -q )
+if [ -z "$AUTH_NETWORK_ID" ]; then
+    echo "Create docker network $AUTH_NETWORK"
+    docker network create --driver bridge $AUTH_NETWORK
+fi
+
 docker-compose -f ${SCRIPT_DIR}/docker-compose-test.yml up -d db
 sleep 2
 docker-compose -f ${SCRIPT_DIR}/docker-compose-test.yml run --rm auth-api bin/rails db:setup
 docker-compose -f ${SCRIPT_DIR}/docker-compose-test.yml up -d auth-api
 docker-compose -f ${SCRIPT_DIR}/docker-compose-test.yml run --rm users-api bin/rails db:setup
 docker-compose -f ${SCRIPT_DIR}/docker-compose-test.yml up -d users-api
+
+AUTH_API_CONTAINER=$( docker-compose -f ${SCRIPT_DIR}/docker-compose-test.yml ps -q auth-api )
+docker network connect --alias auth-api $AUTH_NETWORK $AUTH_API_CONTAINER
+USERS_API_CONTAINER=$( docker-compose -f ${SCRIPT_DIR}/docker-compose-test.yml ps -q users-api )
+docker network connect --alias users-api $AUTH_NETWORK $USERS_API_CONTAINER
